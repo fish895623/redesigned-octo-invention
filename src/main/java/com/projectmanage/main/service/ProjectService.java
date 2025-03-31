d5fac1761803aaf8e443e5353c81bce4d63c492a
somecreater <dbstjrgy123@gmail.com>
2025-03-31 17:57:12 +0900

    리포지터리 인터페이스 추가 및 프로젝트 서비스 클래스 생성

---
+++ src/main/java/com/projectmanage/main/service/ProjectService.java
@@ -0,0 +1,23 @@
+package com.projectmanage.main.service;
+
+import com.projectmanage.main.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+
+    public final ProjectRepository projectRepository;
+
+    //특정 회원의 프로젝트 목록 읽기
+
+    //프로젝트 하나 읽기
+
+    //프로젝트 생성
+
+    //프로젝트 수정
+
+    //프로젝트 삭제
+
+}
