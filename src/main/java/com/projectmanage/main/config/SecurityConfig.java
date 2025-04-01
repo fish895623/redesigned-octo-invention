36455497406ea1dc8d42062119a8774b4d20463c
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-04-01 15:32:06 +0900

    refactor: Simplify CORS configuration and update authorization rules in SecurityConfig for improved readability and maintainability

--- src/main/java/com/projectmanage/main/config/SecurityConfig.java
+++ src/main/java/com/projectmanage/main/config/SecurityConfig.java
@@ -34,41 +34,34 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(corsCustomizer -> corsCustomizer
-                .configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(@NonNull HttpServletRequest request) {
-                        CorsConfiguration configuration = new CorsConfiguration();
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                        configuration.addExposedHeader("Set-Cookie");
-                        return configuration;
-                    }
-                }));
-
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(@NonNull HttpServletRequest request) {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                configuration.setAllowedMethods(Collections.singletonList("*"));
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                configuration.setMaxAge(3600L);
+                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                configuration.addExposedHeader("Set-Cookie");
+                return configuration;
+            }
+        }));
         http.csrf((auth) -> auth.disable());
-
         http.formLogin((auth) -> auth.disable());
-
         http.httpBasic((auth) -> auth.disable());
-
         http.addFilterBefore(new JWTFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
-
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/api/auth/login").permitAll()
-                .requestMatchers("/api/auth/register").permitAll()
-                .requestMatchers("/api/auth/status").permitAll()
-                .requestMatchers("/api/auth/user").permitAll()
-                .requestMatchers("/my").hasRole("USER")
-                .anyRequest().authenticated());
-
+        http.authorizeHttpRequests((auth) -> {
+            auth.requestMatchers("/").permitAll()
+                    .requestMatchers("/api/projects").permitAll()
+                    .requestMatchers("/api/auth/login").permitAll()
+                    .requestMatchers("/api/auth/register").permitAll()
+                    .requestMatchers("/api/auth/status").permitAll()
+                    .requestMatchers("/api/auth/user").permitAll()
+                    .requestMatchers("/my").hasRole("USER");
+        });
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
         return http.build();
     }
 
