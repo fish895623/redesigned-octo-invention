38c6198508bdf12c7f39c3c3fabe7dcb1dca02ec
somecreater <127456520+somecreater@users.noreply.github.com>
2025-04-01 10:23:57 +0900

    dto, entity 변환 클래스 및 코드 작성

---
+++ src/main/java/com/projectmanage/main/model/builder/MilestoneBuilder.java
@@ -0,0 +1,59 @@
+package com.projectmanage.main.model.builder;
+
+import com.projectmanage.main.model.Milestone;
+import com.projectmanage.main.model.dto.MilestoneDTO;
+import com.projectmanage.main.repository.ProjectRepository;
+import com.projectmanage.main.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MilestoneBuilder {
+    public final TaskBuilder taskBuilder;
+    public final ProjectRepository projectRepository;
+    public final TaskRepository taskRepository;
+
+    public MilestoneDTO toDTO(Milestone milestone) {
+        MilestoneDTO dto = new MilestoneDTO();
+        dto.setId(milestone.getId());
+        dto.setTitle(milestone.getTitle());
+        dto.setDescription(milestone.getDescription());
+
+        if (milestone.getProject() != null) {
+            dto.setProjectId(milestone.getProject().getId());
+        }
+
+        dto.setStartDate(milestone.getStartDate());
+        dto.setDueDate(milestone.getDueDate());
+        dto.setCompleted(milestone.isCompleted());
+
+        dto.setTasks(milestone.getTasks().stream().map(taskBuilder::toDTO).toList());
+
+        dto.setCreatedAt(milestone.getCreatedAt());
+        dto.setUpdatedAt(milestone.getUpdatedAt());
+
+        return dto;
+    }
+
+    public Milestone toEntity(MilestoneDTO milestone){
+        Milestone entity = new Milestone();
+        entity.setId(milestone.getId());
+        entity.setTitle(milestone.getTitle());
+        entity.setDescription(milestone.getDescription());
+
+        if (milestone.getProjectId()!= null) {
+            entity.setProject(projectRepository.findById(milestone.getProjectId()).orElse(null));
+        }
+
+        entity.setStartDate(milestone.getStartDate());
+        entity.setDueDate(milestone.getDueDate());
+        entity.setCompleted(milestone.isCompleted());
+
+        entity.setTasks(taskRepository.findByMilestone_Id(milestone.getId()));
+
+        entity.setCreatedAt(milestone.getCreatedAt());
+        entity.setUpdatedAt(milestone.getUpdatedAt());
+        return entity;
+    }
+}
--- src/main/java/com/projectmanage/main/repository/MilestoneRepository.java
+++ src/main/java/com/projectmanage/main/repository/MilestoneRepository.java
@@ -4,7 +4,10 @@ import com.projectmanage.main.model.Milestone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MilestoneRepository  extends JpaRepository<Milestone, Long> {
 
+    List<Milestone> findByProject_Id(Long id);
 }
