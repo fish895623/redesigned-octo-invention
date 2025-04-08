36b48e20b1c3b9181baa6a0f419ed78983706c84
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-04-08 11:41:39 +0900

    feat: add CommentRepository for task comments management

---
+++ src/main/java/com/projectmanage/main/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.projectmanage.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.projectmanage.main.model.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
